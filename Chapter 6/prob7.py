9477aa735d5295e063e5072203f91a74d8af1cd1
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-22 16:30:23 +0500

    Create prob7.py

---
+++ Chapter 6/prob7.py
@@ -0,0 +1,6 @@
+post = 'hi SaAd bhia hlu assalamu alaikum from makka'
+
+if ("SaaD".lower() in post.lower()):
+    print('This post is talking about Saad')
+else:
+    print('This post is not talking about Saad')
