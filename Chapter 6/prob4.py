b7a6bffe535d6ca4cb7dcb98a9de53d3ebe41048
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-22 15:46:52 +0500

    Update prob4.py

--- Chapter 6/prob4.py
+++ Chapter 6/prob4.py
@@ -2,5 +2,5 @@ name = input('Enter your username under 10 characters: ')
 a = len(name)
 if(a < 10):
     print('Yes : this is under 10 characters')
-elif( a >= 10):
+else:
     print('No : there are more characters then 10')
