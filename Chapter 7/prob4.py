02fd087d5762d6c4485ae17aa93a2a73d3e95e8a
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-24 15:19:02 +0500

    Update prob4.py

--- Chapter 7/prob4.py
+++ Chapter 7/prob4.py
@@ -1,3 +1,8 @@
-num = int(input('Enter a number for prim checking'))
+num = int(input('Enter a number for prim checking: '))
 
-if(num % )
+for i in range(2, num):
+    if(num % i == 0):
+        print(f'{num} is not a prime number')
+        break
+else:
+    print(f'{num} is a prim number')
