1843d1a8daca42c1ad61b90de6daaaa3e1928295
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-24 14:10:37 +0500

    Update for-iteration.py

--- Chapter 7/for-iteration.py
+++ Chapter 7/for-iteration.py
@@ -1,12 +1,14 @@
-# for loop in list
+print('for loop in list')
 l = [1, 234, 678, 543]
 for i in l:
     print(i)
-# for loop in tuple
+
+print('for loop in tuple')
 t = (99, 45, 22, 1010)
 for i in t:
     print(i)
-# for loop in string
+
+print('for loop in string')
 s = 'Muhammad-Saad'
 for i in s:
     print(i)
