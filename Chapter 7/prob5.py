501b664b8e55d2d8b2c92a4ee9f963da49b833bd
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-24 17:18:18 +0500

    Update prob5.py

--- Chapter 7/prob5.py
+++ Chapter 7/prob5.py
@@ -1,3 +1,5 @@
+# using for loop
+print('Sum using for loop')
 n = int(input('Enter a number for sum: '))
 sum = 0
 for i in range(1, n+1):
@@ -7,9 +9,11 @@ print(sum)
 
 
 # using while loop
+print('Sum using while loop')
+m = int(input('Enter a number for sum: '))
 sum = 0
 j = 0
-while(j<=n):
+while(j<=m):
     sum += j
     j += 1
 print(sum)
