74723d21690e8b89aee53fb9edff5d4b700bca83
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-24 17:27:49 +0500

    Create prob6.py

---
+++ Chapter 7/prob6.py
@@ -0,0 +1,13 @@
+# using for loop
+print('using for loop')
+
+n = int(input('Enter a number for Factorial: '))
+
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print(fact)
+
+
+# using while loop
+print('using while loop')
