575a61454948794ef25bee40c62fb85cd8e8616a
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-08-31 12:44:41 +0500

    Create prob1.py

---
+++ Chapter 9/prob1.py
@@ -0,0 +1,8 @@
+f = open("poem.txt")
+content = f.read()
+if("twinkle".lower() in content):
+    print("the word twinkle is present in the content")
+else:
+    print("the word twinkle is not present in the content")
+
+f.close()
