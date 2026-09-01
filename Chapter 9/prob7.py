837288d0dfa892d69c180f7e49c267eba470db6b
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-09-01 17:45:35 +0500

    Update prob7.py

--- Chapter 9/prob7.py
+++ Chapter 9/prob7.py
@@ -4,7 +4,7 @@ with open("log.txt") as f:
 lineno = 1
 for line in lines:
     if("python" in line):
-        print(f"Yes Pythin is present at line no : {lineno}")
+        print(f"Yes Python is present at line no : {lineno}")
         break
     lineno += 1
 else:
