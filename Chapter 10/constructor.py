bfd8403eb7b98fcc5cdc8990cb5076d75535f0a2
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-09-11 17:31:18 +0500

    Create constructor.py

---
+++ Chapter 10/constructor.py
@@ -0,0 +1,17 @@
+class employee:
+    language = "py"  # This is a class attribute
+    salary = 1300000
+
+    def __init__(self):
+        print("this is a constructore running")
+
+    def get_info(self):
+        print(f"The language is {self.language} and salary is {self.salary}")
+    @staticmethod # it prevent to get the self perameter as a whole object 
+    def greet():
+        print("hello, i am greeting you")
+
+
+saad = employee()
+print(saad.language, saad.salary)
+rohaan = employee()
