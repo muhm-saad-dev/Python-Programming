884cb6d7941a21a2fd17039128c64a35abc007b9
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-09-16 10:37:40 +0500

    Update prob2.py

--- Chapter 10/prob2.py
+++ Chapter 10/prob2.py
@@ -8,6 +8,10 @@ class Calculator:
     def cube(self):
         print(f"The cube is {self.n * self.n * self.n}")
 
-    def squar(self):
-        print(f"The squar is {self.n ** 1/2}")
+    def squarRoot(self):
+        print(f"The squarRoot is {self.n ** 1/2}")
 
+a = Calculator(4)
+a.squar()
+a.cube()
+a.squarRoot()
