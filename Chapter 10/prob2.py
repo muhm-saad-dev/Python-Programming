1fcca9d12aa584a1b14e01ae9d30a21493f1388b
saadfaisal803-hub <saadfaisal803@gmail.com>
2026-09-16 10:36:02 +0500

    Create prob2.py

---
+++ Chapter 10/prob2.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def __init__(self, n):
+        self.n = n
+
+    def squar(self):
+        print(f"The squar is {self.n * self.n}")
+
+    def cube(self):
+        print(f"The cube is {self.n * self.n * self.n}")
+
+    def squar(self):
+        print(f"The squar is {self.n ** 1/2}")
+
